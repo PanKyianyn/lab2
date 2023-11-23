75528e263ef96ea1efb607c24a583733daf8054f
PanKyianyn <101899565+PanKyianyn@users.noreply.github.com>
2023-11-23 19:59:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return f"<p>Today is {date.today().strftime('%d/%m/%Y')}</p>"
+     # Import datetime class from datetime module
+    from datetime import datetime
+ 
+    dayofweek=""
+    # returns current date and time
+    numdayofweek = datetime.now().weekday()
+    if (numdayofweek == 0): dayofweek="Monday"
+    if (numdayofweek == 1): dayofweek="Tuesday"
+    if (numdayofweek == 2): dayofweek="Wednesday"
+    if (numdayofweek == 3): dayofweek="Thursday"
+    if (numdayofweek == 4): dayofweek="Friday"
+    if (numdayofweek == 5): dayofweek="Saturday"
+    if (numdayofweek == 6): dayofweek="Sunday"
+    #print("**********")
+    now = datetime.now().strftime("Today:"+"\n"+dayofweek+"\n"+"%d/%m/%Y"+"\n"+"Time:"+"\n"+"%H:%M:%S")
+    #print(now)
+    #print("**********")
+
+    return now
+    
