880e53bf803ea094c7e82c266498cce12afd2c32
PanKyianyn <101899565+PanKyianyn@users.noreply.github.com>
2023-11-23 21:02:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def hello_world():
     if (numdayofweek == 5): dayofweek="Saturday"
     if (numdayofweek == 6): dayofweek="Sunday"
     #print("**********")
-    now = datetime.now().strftime("Today:"+"/n"+dayofweek+"/n"+"%d/%m/%Y"+"/n"+"Time:"+"/n"+"%H:%M:%S")
+    now = datetime.now().strftime("Today:  "+""+dayofweek+"  "+"%d/%m/%Y"+""+"Time:  "+""+"%H:%M:%S")
     #print(now)
     #print("**********")
 
